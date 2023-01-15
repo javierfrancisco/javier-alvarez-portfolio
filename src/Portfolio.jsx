0127026a9a91edf53583a26f1a317cf5c8cb298e
zombie <javierfrancisco@gmail.com>
2023-01-14 22:34:06 -0500

    Update formatting

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -2,12 +2,18 @@ const Portfolio = () => {
   return (
     <div>
       <section id="intro">
-        <p>
-          Hi, my name is <span>Javier Alvarez</span>.
+        <p className="name">
+          Hi, my name is <span>Javier Alvarez.</span>
         </p>
         <h2>I develop, create and design digital experiences.</h2>
         <p>I'm a developer specializing in HTML, CSS, JS.</p>
-        <p>Currently, I'm working at Discover. You can follow me on <a href="https://www.linkedin.com/in/javier-alvarez-35528836/">LinkedIn</a>.</p>
+        <p>
+          Currently, I'm working at Discover. You can follow me on{" "}
+          <a href="https://www.linkedin.com/in/javier-alvarez-35528836/">
+            LinkedIn
+          </a>
+          .
+        </p>
       </section>
     </div>
   );
