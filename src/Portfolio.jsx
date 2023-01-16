45a4f8735f8c4498f674cd048770780a792a81fb
zombie <javierfrancisco@gmail.com>
2023-01-15 20:14:15 -0500

    Add nav bar

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,6 +1,49 @@
 const Portfolio = () => {
   return (
     <div>
+      <section id="navBar">
+        <nav>
+          <ul>
+            <li>
+              <h1>
+                <a href="index.html">
+                  <span class="fa-solid fa-rocket" aria-hidden="true"></span>
+                  <span>Javier Alvarez</span>
+                </a>
+              </h1>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="about.html">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="#projects">
+                <span class="fa-brands fa-linkedin" aria-hidden="true"></span>
+                <span class="sr-only">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/javierfrancisco" target="_blank">
+                <span
+                  class="fa-brands fa-square-github"
+                  aria-hidden="true"
+                ></span>
+                <span class="sr-only">Github</span>
+              </a>
+            </li>
+            <li>
+              <a href="#projects" target="_blank">
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
       <section id="intro">
         <p className="name">
           Hi, my name is <span>Javier Alvarez.</span>
@@ -13,6 +56,17 @@ const Portfolio = () => {
             LinkedIn
           </a>
           .
+        </p>
+      </section>
+
+      <section id="contact">
+        <h2>Contact me</h2>
+        <p className="contact">
+          I'm always interested in hearing about new teaching opportunities,
+          writing curricula, or instructional design work.
+        </p>
+        <p>
+          <a href="mailto:javierfrancisco@gmail.com">Email me</a>
         </p>
       </section>
     </div>
