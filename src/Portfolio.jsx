d87bcb5a4aaf20a7fe5cdb62182d71702c0cae84
zombie <javierfrancisco@gmail.com>
2023-01-24 21:33:23 -0500

    Add projects styling

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -61,27 +61,29 @@ const Portfolio = () => {
       <div className="section-dkblue">
       <section id="projects">
         <h2>Projects I'm proud of</h2>
-        <article>
-        <h4>Latest Project</h4>
-        <h3>Guided Product Selector</h3>
-        <p className="black-box">Description of the project. This should be fairly concise while also describing the key components
-          that you developed or worked on. It can be as long as you need it to
-          be but should at least be a few sentences long. Be sure to include
-          specific links anywhere in the description. A link looks like
-          <a href="https://frontendmasters.github.io/grid-flexbox-v2/">this</a>,
-          and multiple links look <a href="#">like this</a> and{" "}
-          <a href="#">like this</a>.
-        </p>
-        <h4>Technologies used include:</h4>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>SVG</li>
-        </ul>
-        <img
-          src="https://assets.codepen.io/296057/fem-gettingstartedcss-ch5-1.png"
-          alt="Screenshot of the Wall of Wonder."
-        />
+          <article>
+            <div className="text">
+              <h4>Latest Project</h4>
+              <h3>Guided Product Selector</h3>
+              <p className="black-box">Description of the project. This should be fairly concise while also describing the key components
+              that you developed or worked on. It can be as long as you need it to
+              be but should at least be a few sentences long. Be sure to include
+              specific links anywhere in the description. A link looks like
+              <a href="https://frontendmasters.github.io/grid-flexbox-v2/">this</a>,
+              and multiple links look <a href="#">like this</a> and{" "}
+              <a href="#">like this</a>.
+              </p>
+              <h4>Technologies used include:</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SVG</li>
+              </ul>
+            </div>
+            <img
+              src="https://assets.codepen.io/296057/fem-gettingstartedcss-ch5-1.png"
+              alt="Screenshot of the Wall of Wonder."
+            />
         </article>
       </section>
       </div>
